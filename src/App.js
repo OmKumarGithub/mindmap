9a5890fea15cbd19cabad5487ca7b0558e2870a5
OmKumarGithub <omkumar742003@gmail.com>
2024-06-30 08:37:07 +0530

    added mindmaps

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About  from "./pages/About";
 import Dashboard from "./pages/Dashboard";
+import Mindmaps from "./pages/Mindmaps";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
@@ -20,6 +21,7 @@ function App() {
     <Route path="/contact" element={<Contact />} />
     <Route path="/about" element={<About />} />
     <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/mindmaps" element={<Mindmaps></Mindmaps>} />
     </Route>
     </Routes>
   </BrowserRouter>
