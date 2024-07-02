371392b5f5eac4ac704d3b4d6d8801185d51490c
OmKumarGithub <omkumar742003@gmail.com>
2024-07-02 13:12:36 +0530

    modified mindmap page

--- frontend/src/component/File.js
+++ frontend/src/component/File.js
@@ -1,127 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faNetworkWired,faCheck} from "@fortawesome/free-solid-svg-icons";
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
-
-const Folder = (props) => {
-
-  const navigate = useNavigate()
-  const [name, setname] = useState("");
-  const [issubmit,setissubmit]=useState(false)
-  const [showDiv, setShowDiv] = useState(true);
-const count =props.count 
-  const handleInputChange = (e) => {
-    setname(e.target.value);
-    
-  };
-
-  const linker =()=>{
-    navigate("/workspace")
-  }
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    setissubmit(true)
-   
-    
-  setTimeout(() => {setShowDiv(!showDiv);}, 1000); 
-
-
-
-
-
-
-
- 
-    
-    const data = {
-      name,
-  
-    };
-    
-    
-    
-    axios
-      .post('http://localhost:5000/api/file', data)
-      .then((response) => {
-    
-        if(response.data.msg == "success"){
-           
-        console.log("ok")
-        }
-        else if (response.data.msg == "fail" ){
-            // setfail(true)
-        }
-    
-        // if (response.data.success) {
-        //   setResponseMessage('Form submitted successfully');
-        //   setSuccess(true)
-        // } else {
-        //   setResponseMessage('Form submission failed');
-        // }
-      })
-      .catch((error) => {
-        console.error(error);
-        
-      }
-      );
-    
-    
-
-
-
-
-
-
-
-
-
-  } 
-  props.getdata(issubmit)
+export const File = ({demo}) => {
   return (
-    <>
-      <div  className="flex items-center flex-col justify-center rounded bg-gray-50 h-28 dark:bg-gray-800 cursor-pointer">
-        <p className="text-2xl text-gray-400 dark:text-gray-500 ">
-        
-         
-         
-         
-    {issubmit && showDiv?(      <span class="relative flex h-3 w-3 mb-3">
-  <span class="animate-ping absolute inline-flex h-full w-full rounded-full  opacity-75 "><FontAwesomeIcon  icon={faCheck} /></span>
-  </span>
-    ):(
-      <FontAwesomeIcon onClick={linker} icon={faNetworkWired} className='scale-[2]'/>
-
-
-    )}
-
-        </p>
-        {/* <span className='mt-2 dark:text-white '>New Folder {count}</span> */}
-        <form 
-        className='flex gap-2 '
-        onSubmit={handleSubmit}
+      <div className="p-8 border  m-4 p-5 inline-block shadow-2xl rounded-md">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="80px"
+          height="80px"
+          viewBox="0 0 24 24"
+          fill="none"
         >
-        <label htmlFor="foldername"></label>
-        <input
-          className={`block text-base py-1 px-4 rounded-lg mt-4 w-36  bg-white border-2  text-center focus:border cursor-pointer  }`}
-          type="text"
-          autoComplete="off"
-          required
-          value={name}
-          onChange={handleInputChange}
-          placeholder='Mindmap Name'
-     
-        />
-    
-        <button 
-       
-        className='p-1 text-sm mr-2 border-2  rounded-md mt-3 l-3 cursor-pointer bg-blue-600 text-white'>save</button>
-        </form>
+          <g opacity="0.5">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M14 22H10C6.22876 22 4.34315 22 3.17157 20.8284C2 19.6569 2 17.7712 2 14V10C2 6.22876 2 4.34315 3.17157 3.17157C4.34315 2 6.23869 2 10.0298 2C10.6358 2 11.1214 2 11.53 2.01666C11.5166 2.09659 11.5095 2.17813 11.5092 2.26057L11.5 5.09497C11.4999 6.19207 11.4998 7.16164 11.6049 7.94316C11.7188 8.79028 11.9803 9.63726 12.6716 10.3285C13.3628 11.0198 14.2098 11.2813 15.0569 11.3952C15.8385 11.5003 16.808 11.5002 17.9051 11.5001L18 11.5001H21.9574C22 12.0344 22 12.6901 22 13.5629V14C22 17.7712 22 19.6569 20.8284 20.8284C19.6569 22 17.7712 22 14 22Z"
+              fill="#1C274C"
+            />
+          </g>
+          <path
+            d="M6 13.75C5.58579 13.75 5.25 14.0858 5.25 14.5C5.25 14.9142 5.58579 15.25 6 15.25H14C14.4142 15.25 14.75 14.9142 14.75 14.5C14.75 14.0858 14.4142 13.75 14 13.75H6Z"
+            fill="#1C274C"
+          />
+          <path
+            d="M6 17.25C5.58579 17.25 5.25 17.5858 5.25 18C5.25 18.4142 5.58579 18.75 6 18.75H11.5C11.9142 18.75 12.25 18.4142 12.25 18C12.25 17.5858 11.9142 17.25 11.5 17.25H6Z"
+            fill="#1C274C"
+          />
+          <path
+            d="M11.5092 2.2601L11.5 5.0945C11.4999 6.1916 11.4998 7.16117 11.6049 7.94269C11.7188 8.78981 11.9803 9.6368 12.6716 10.3281C13.3629 11.0193 14.2098 11.2808 15.057 11.3947C15.8385 11.4998 16.808 11.4997 17.9051 11.4996L21.9574 11.4996C21.9698 11.6552 21.9786 11.821 21.9848 11.9995H22C22 11.732 22 11.5983 21.9901 11.4408C21.9335 10.5463 21.5617 9.52125 21.0315 8.79853C20.9382 8.6713 20.8743 8.59493 20.7467 8.44218C19.9542 7.49359 18.911 6.31193 18 5.49953C17.1892 4.77645 16.0787 3.98536 15.1101 3.3385C14.2781 2.78275 13.862 2.50487 13.2915 2.29834C13.1403 2.24359 12.9408 2.18311 12.7846 2.14466C12.4006 2.05013 12.0268 2.01725 11.5 2.00586L11.5092 2.2601Z"
+            fill="#1C274C"
+          />
+        </svg>
+        <div>{demo}</div>
       </div>
-    </>
-  );
+  )
 }
-
-export default Folder;
--- frontend/src/pages/Mindmaps.js
+++ frontend/src/pages/Mindmaps.js
@@ -1,105 +1,104 @@
-import React, { useEffect, useState } from "react";
-// import Folder from "../Components/Folder";
-import File from "../component/File";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFolder, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+"use client";
 
-const Mindmaps = () => {
-  const [count, setCount] = useState(0);
-  const [toadd, setToAdd] = useState(false);
-  const [showDiv, setShowDiv] = useState(true);
-  const [issubmit, setIsSubmit] = useState(false);
-  const [array, setArray] = useState([{ id: 0, name: "" }]);
+import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
+import { useEffect, useState } from "react";
+import {File} from '../component/File'
 
-  // useEffect(() => {
-  //   // Fetch the array from the database
-  //   fetch("http://localhost:5000/api/user")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(data)
-  //       // setArray(data);
-  //     })
-  //     .catch((error) => {
-  //       console.log("Error:", error);
-  //     });
-  // }, []);
+function Mindmaps() {
+  const [openModal, setOpenModal] = useState(false);
+  const [mindmapName, setmindmapName] = useState("");
+  const [showfile , setshowfile] = useState(false)
+  const [filenames,setfilenames] =useState(['name1' ,'name2' , 'name3'])
 
-  const getdata = (e) => {
-    console.log(e);
-    setIsSubmit(e);
-  };
+  function onCloseModal() {
+    setOpenModal(false);
+    // setmindmapName("");
+    if(mindmapName ==""){
+     return}
+    setfilenames((prev)=> [...prev , mindmapName])
+    console.log("fvnkjdsbkj");
+    setmindmapName("");
+  }
 
-  const onclick = () => {
-    setToAdd(!toadd);
+  
 
-    if (issubmit && toadd) {
-      const updatedArray = [...array];
-      const num = updatedArray.length;
-      updatedArray.push({ id: num + 1 });
-      setCount(count + 1);
-      setArray(updatedArray);
-    } else {
-      setTimeout(() => {
-        setShowDiv(!showDiv);
-      }, 2000);
-    }
-  };
+  function addmindmapFile() {
+     if(mindmapName !=""){
+      onCloseModal()}
+  }
 
   return (
     <>
-      {!issubmit && toadd && showDiv ? (
-        <div
-          className="p-4 mb-4 text-sm z-50 text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-          role="alert"
-        >
-          <span class="font-medium">Wrong Name!</span>
-          Give file a proper Name.
-        </div>
-      ) : (
-        <></>
-      )}
-      <div className="bg-gradient-to-r from-gray-50 to-gray-300 dark:bg-gradient-to-r dark:from-gray-700 dark:to-gray-500 h-screen">
-        <div className="text-xl   cursor-pointer p-2 m-5 border-gray-300 border-2 shadow-md">
-          <Link to="/dashboard">
-            <FontAwesomeIcon icon={faArrowLeft} /> Go back To Dashboard
-          </Link>{" "}
-        </div>
-        <div className="flex justify-center items-center ">
-          <div className="p-4 border-2 border-gray-800 border-dashed rounded-lg dark:border-white">
-            <div className="grid grid-cols-2 gap-4">
-              {array.map((arr) => (
-                <File getdata={getdata}></File>
-              ))}
-              <div
-                onClick={onclick}
-                className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800 cursor-pointer hover:"
-              >
-                <p className=" flex flex-col items-center gap-3 text-2xl text-gray-400 dark:text-gray-500">
-                  <div>Add new file </div>
-                  <svg
-                    className="w-3.5 h-3.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 1v16M1 9h16"
-                    />
-                  </svg>
-                </p>
+    {/* ************modal */}
+    <div class="flex justify-center items-center z-10">
+      <Modal show={openModal}  className="w-1/3 absolute center bottom-10"size="md" onClose={onCloseModal} popup>
+        <Modal.Header />
+        <Modal.Body>
+          <div className="space-y-6">
+            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+              Mindmap Name
+            </h3>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="Text" value="Your Mindmap Name" />
               </div>
+              <TextInput
+                id="Text"
+                placeholder="Mindmap Name"
+                value={mindmapName}
+                className=" "
+                onChange={(event) => setmindmapName(event.target.value)}
+                required
+              />
+            </div>
+            <div className="">
+              <Button onClick={addmindmapFile}>Add Mindmap</Button>
             </div>
           </div>
+        </Modal.Body>
+      </Modal>
+      </div>
+
+
+
+{/* ***********************************modal ends **************************** */}
+
+
+      <div className="h-screen w-screen bg-gradient-to-r dark:from-gray-700 dark:to-gray-500">
+      {/* *************panel starts************************** */}
+        <div
+          id="panel"
+          className="sticky relative cursor-pointer hover:scale-105 p-2  bg-blue-700 hover:bg-blue-700 inline-block  m-2 box-shadow-md rounded-md border-black"
+        >
+          <svg
+            onClick={() => setOpenModal(true)}
+            xmlns="http://www.w3.org/2000/svg"
+            width="40px"
+            height="40px"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M17 19H21M19 17V21M13 3H8.2C7.0799 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.0799 21 8.2 21H12M13 3L19 9M13 3V7.4C13 7.96005 13 8.24008 13.109 8.45399C13.2049 8.64215 13.3578 8.79513 13.546 8.89101C13.7599 9 14.0399 9 14.6 9H19M19 9V12M9 17H12M9 13H15M9 9H10"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
+{/* *************panel ends************************** */}
+
+
+
+        <div className=" h-96 border shadow-white shadow-md bg-zinc-100 mx-20 rounded-lg bg-gray-300 overflow-auto flex-wrap">
+        {filenames.map((name) => (
+    <File key={name} demo={name} />
+  ))}
         </div>
       </div>
     </>
   );
-};
+}
 
 export default Mindmaps;
